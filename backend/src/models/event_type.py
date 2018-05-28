822cfaf6a8b58146210e2a27971b9daa4fd3d378
northcott-j <northcott.j@chisigma.co>
2018-05-28 11:26:35 -0400

    adjusting daily drink count to 12h

--- backend/src/models/event_type.py
+++ backend/src/models/event_type.py
@@ -4,7 +4,7 @@ from orator.orm import has_many
 
 class EventType(model.Model):
 	# (Name for the frontend, window for querying the db)
-	COUNT_WINDOWS = [('Past Day', '24h'), ('Past Week', '7d'), ('All Time', '*')]
+	COUNT_WINDOWS = [('Past Day', '12h'), ('Past Week', '7d'), ('All Time', '*')]
 
 	@has_many
 	def events(self):
